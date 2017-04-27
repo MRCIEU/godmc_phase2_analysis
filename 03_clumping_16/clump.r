0ac767f7435a98bfeb2f6e2448f31347bae48cac
Gibran Hemani <explodecomputer@gmail.com>
2017-04-27 21:32:09 +0100

    added clumping

--- 03_clumping_16/clump.r
+++ 03_clumping_16/clump.r
@@ -1,47 +1,76 @@
 library(tidyverse)
-
-i <- 1
-
-
 do_clump <- function(pval_file, bfile, pval_threshold, rsq_threshold, kb_threshold)
 {
 	cmd <- paste0(
 		"module add apps/plink-1.90b3v;",
 		"plink --bfile ", bfile, 
-		"--clump ", pval_file,
-		"--clump-p1 ", pval_threshold,
-		"--clump-r2 ", rsq_threshold,
-		"--clump-kb ", kb_threshold,
-		"--out ", pval_file
+		" --clump ", pval_file,
+		" --clump-p1 ", pval_threshold,
+		" --clump-r2 ", rsq_threshold,
+		" --clump-kb ", kb_threshold,
+		" --out ", pval_file
 	)
-	res <- read.table(pval_file, header=TRUE, stringsAsFactors=FALSE)
+	system(cmd)
+	fn <- paste0(pval_file, ".clumped")
+	if(!file.exists(fn)) return(NULL)
+
+	res <- read.table(fn, header=TRUE, stringsAsFactors=FALSE)
+	unlink(paste0(pval_file, "*"))
 	return(res$SNP)
 }
 
 
+##
+
+arguments <- commandArgs(T)
+i <- as.numeric(arguments[1])
+out <- arguments[2]
+
+bfile <- "/panfs/panasas01/shared-godmc/1kg_reference_ph3/eur"
+
+##
+
+# Get cpg positions
+load("cpg_pos.rdata")
+
 a <- read_tsv(paste0("../results/16/16_", i, ".txt.gz"))
 a <- a %>% separate(MarkerName, into=c("snp", "cpg"), sep="_")
-
+a$snp2 <- a$snp
+a <- a %>% separate(snp2, into=c("snpchr", "snppos", "snptype"), sep=":")
+a$snppos <- as.numeric(a$snppos)
+a <- inner_join(a, cpgpos, by=c("cpg"))
+a$cis <- FALSE
+cis_radius <- 1000000
+a$cis[a$snpchr == a$cpgchr & (abs(a$snppos - a$cpgpos) <= cis_radius)] <- TRUE
 
 # Split into cis and trans
-
 # Apply different thresholds to cis and trans
-
 # e.g. 1e-8 for cis
 # e.g. 1e-14 for trans
 
 
-a <- filtergroup_by(a, cpg) %>%
+clumped <- group_by(a, cpg, cis) %>%
 	do({
 		x <- .
+
 		# write clump file
 		cpgname <- x$cpg[1]
+		fn <- paste0(cpgname, "_", i, ".txt")
+		unlink(paste0(fn, "*"))
+
 		y <- x[, c("snp", "P-value")]
 		names(y) <- c("SNP", "P")
-		write.table(y, row=FALSE, col=TRUE, qu=FALSE)
+		write.table(y, file=fn, row=FALSE, col=TRUE, qu=FALSE)
 
+		# Get cis/trans clumping threshold
+		thresh <- ifelse(x$cis[1], 1e-4, 5e-8)
+		keep <- do_clump(fn, bfile, thresh, 0.0001, 2500)
 
-
+		x <- subset(x, snp %in% keep)
+		return(x)
 	})
+
+save(clumped, file=out)
+
 
 
