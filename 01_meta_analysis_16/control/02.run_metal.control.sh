30879216bdfabfaab2dd33b476bde2d91bd35a6c
Josine Min <josine.min@bristol.ac.uk>
2017-05-22 17:01:09 +0100

    control dir

---
+++ 01_meta_analysis_16/control/02.run_metal.control.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+#PBS -N meta_16
+#PBS -o job_reports/meta_16-output
+#PBS -e job_reports/meta_16-error
+#PBS -t 1-962
+#PBS -l walltime=02:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -S /bin/bash
+
+
+
+set -e
+
+# Need to gunzip the results_16.tgz for each cohort
+# Takes about 2 minutes to create each GWAMA file for each cohort
+
+
+echo "Running on ${HOSTNAME}"
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  PBS_ARRAYID=${1}
+fi
+
+i=${PBS_ARRAYID}
+
+
+
+cd /panfs/panasas01/shared-godmc/godmc_phase2_analysis/01_meta_analysis_16
+
+cohort_dir="../data/16/"
+metal_dir="../scratch/control/16_${i}"
+result_dir="../results/16/control"
+
+metal_in="16_${i}.in"
+
+mkdir -p ${result_dir}
+mkdir -p ${metal_dir}
+rm -f ${metal_dir}/${metal_in}
+touch ${metal_dir}/${metal_in}
+
+
+
+echo "OUTFILE 16_${i} .txt" >> ${metal_dir}/${metal_in}
+echo "" >> ${metal_dir}/${metal_in}
+
+echo "MARKER MARKERNAME" >> ${metal_dir}/${metal_in}
+echo "ALLELE EA NEA" >> ${metal_dir}/${metal_in}
+echo "EFFECT BETA" >> ${metal_dir}/${metal_in}
+echo "FREQ EAF" >> ${metal_dir}/${metal_in}
+echo "EFFECT BETA" >> ${metal_dir}/${metal_in}
+echo "STDERR SE" >> ${metal_dir}/${metal_in}
+echo "WEIGHT N" >> ${metal_dir}/${metal_in}
+echo "CUSTOMVARIABLE TotalSampleSize" >> ${metal_dir}/${metal_in}
+echo "LABEL TotalSampleSize as N" >> ${metal_dir}/${metal_in}
+echo "SCHEME STDERR" >> ${metal_dir}/${metal_in}
+echo "AVERAGEFREQ ON" >> ${metal_dir}/${metal_in}
+echo "" >> ${metal_dir}/${metal_in}
+
+#CHR MARKERNAME EA MODEL N SE BETA P
+
+for cohort in ${cohort_dir}*16.tar
+do
+
+	echo $cohort
+	cohortname=$(basename "$cohort" .tar)
+#	mkdir -p ${cohortname}_${i}
+#	cd ${cohortname}_${i}
+#	tar xvf ../${cohort} results/16/results_${i}.gz
+#	cd ../
+#	mv ${cohortname}_${i}/results/16/results_${i}.gz ${metal_dir}/${cohortname}_${i}.gz
+#	rm -r ${cohortname}_${i}
+#
+	echo "PROCESS /panfs/panasas01/shared-godmc/godmc_phase2_analysis/scratch/control/${cohortname}.control.merged" >> ${metal_dir}/${metal_in}
+
+done
+
+echo "" >> ${metal_dir}/${metal_in}
+#echo "ANALYZE RANDOM" >> ${metal_dir}/${metal_in}
+echo "ANALYZE" >> ${metal_dir}/${metal_in}
+echo "CLEAR" >> ${metal_dir}/${metal_in}
+echo "QUIT" >> ${metal_dir}/${metal_in}
+
+cp metal ${metal_dir}
+cd ${metal_dir}
+
+./metal ${metal_in}
+mv 16_${i}1.txt 16_${i}.txt
+gzip 16_${i}.txt
+cd -
+mv ${metal_dir}/16_${i}.txt.* ${result_dir}
+rm -r ${metal_dir}
+#
+# GWAMA --filelist ${metal_file} --quantitative
+
---
+++ 01_meta_analysis_16/control/mstat.control.R
@@ -0,0 +1,126 @@
+
+#extract results files
+#results_dir="/projects/MRC-IEU/groups/godmc/meta-analysis/input"
+#metal_in="16_${i}.in"
+#cd $results_dir
+
+#for cohort in ${cohort_dir}*16.tar
+#do
+
+#	echo $cohort
+#	cohortname=$(basename "$cohort" .tar)
+#	echo $cohortname
+#	mkdir -p $results_dir/${cohortname}
+#	cd $results_dir/${cohortname}
+#	tar -xvf ../${cohortname}.tar results/16/results_*.gz
+#	cd $results_dir
+	
+#done
+
+
+
+
+#cd /panfs/panasas01/shared-godmc/godmc_phase2_analysis/01_meta_analysis_16
+
+cohort_dir="/projects/MRC-IEU/groups/godmc/meta-analysis/input"
+result_dir="/panfs/panasas01/shared-godmc/godmc_phase2_analysis/mstat"
+
+metal_dir="../scratch/16_${i}"
+cohort_dir="/panfs/panasas01/shared-godmc/godmc_phase2_analysis/scratch/input"
+library(getmstatistic)  # for calculating M statistics
+library(gridExtra)       # for generating tables
+
+head(heartgenes214)
+#  beta_flipped      gcse          variants studies cases controls fdr214_gwas46
+#1  -0.06325500 0.1607954  chr10:75595440:D      01   278      312             1
+#2  -0.00041045 0.1443252 chr11:100634736:D      01   278      312             1
+#3   0.07529900 0.1415288  chr11:77185247:I      01   278      312             1
+#4   0.06133000 0.1893356  chr12:76386403:I      01   278      312             1
+#5   0.19341000 0.1362752  chr14:75614504:I      01   278      312             1
+#6  -0.03793500 0.1312889  chr16:75308440:D      01   278      312             1
+
+path="/panfs/panasas01/shared-godmc/godmc_phase2_analysis"
+path="/panfs/panasas01/shared-godmc/godmc_phase2_analysis/scratch/control"
+#load(paste0(path,"/results/16/16_clumped.rdata"))
+#dim(clumped)
+#[1] 312024     21
+#a<-clumped
+#a$id<-as.character(paste(a$snp,a$cpg,sep="_"))
+
+#a<-read.table(paste(path,"/results/16/control/16_1.txt.gz",sep=""),he=T)
+#a2<-a[which(a$cis==F & a$Pvalue<5e-8),]
+
+a<-read.table("/panfs/panasas01/sscm/epzjlm/repo/godmc_phase2_analysis/03_clumping_16/clumped.txt",he=T)
+
+#a14.cis.out<-a[which(a$cis==F & a$pval<1e-14),]
+#dim(a14.cis.out)
+#[1] 156857     22
+#o<-order(a14.cis.out$pval)
+#a14.cis.out<-a14.cis.out[o,]
+#probe<-unique(a14.cis.out$cpg)
+#m<-match(probe,a14.cis.out$cpg)
+#a14.cis.out<-a14.cis.out[m,]
+#a14.cis.out<-a[which(a$cis==T & a$pval<1e-14&a$cpgchr=="chr20"),]
+#dim(a14.cis.out)
+#[1] 3213   21
+#dim(data.frame(table(a14.cis.out$snp)))
+#[1] 2497    2
+
+#o<-order(as.numeric(as.character(a14.cis.out$cpgpos)))
+#a14.cis.out<-a14.cis.out[o,]
+
+l<-list.files(path=path,pattern=".control.merged")
+#l2<-list.files(paste(cohort_dir,"/",l[1],"/results/16/",sep=""))
+study<-gsub("_16.control.merged","",l)
+
+res<-data.frame()
+for (i in 1:length(l)){
+#for (i in 1:2){
+cat(i,"\n")
+
+r<-read.table(paste(path,"/",l[i],sep=""),he=T)
+m<-which(r$MARKERNAME%in%as.character(a[,1]))
+
+if(length(m)>0){
+
+r2<-data.frame(r[m,],study=study[i])
+res<-rbind(res,r2)
+}
+}
+
+
+res$study<-as.character(res$study)
+res$SNP<-as.character(res$SNP)
+res$MARKERNAME<-as.character(res$MARKERNAME)
+res$P<-as.numeric(as.character(res$P))
+save.image("/panfs/panasas01/sscm/epzjlm/repo/godmc_phase2_analysis/01_meta_analysis_16/mstatscontrol.RData")
+
+res2<-res[which(res$P<1e-5),]
+
+m<-data.frame(table(res2$MARKERNAME))
+m<-m[which(m$Freq>1),1]
+res2<-res2[res2$MARKERNAME%in%m,]
+
+length(unique(res2$MARKERNAME))
+#[1] 22813
+getmstatistic_results <- getmstatistic(res2$BETA, res2$SE,res2$MARKERNAME, res2$study)
+
+dframe <- getmstatistic_results$M_dataset
+head(dframe)
+
+# Retrieve dataset of stronger than average studies (significant at 5% level)
+getmstatistic_results$influential_studies_0_05
+ 
+# Retrieve dataset of weaker than average studies (significant at 5% level)
+getmstatistic_results$weaker_studies_0_05
+ 
+# Retrieve number of studies and variants
+getmstatistic_results$number_studies
+getmstatistic_results$number_variants
+ 
+# Retrieve expected mean, sd and critical M value at 5% significance level
+getmstatistic_results$M_expected_mean
+getmstatistic_results$M_expected_sd
+getmstatistic_results$M_crit_alpha_0_05
+
+save.image("/panfs/panasas01/sscm/epzjlm/repo/godmc_phase2_analysis/01_meta_analysis_16/mstatscontrol.RData")
