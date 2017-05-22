1dcfe4c2e1717f8288b68c35eda88266234568de
Josine Min <josine.min@bristol.ac.uk>
2017-05-22 17:20:18 +0100

    mstats

---
+++ 01_meta_analysis_16/submit_mstat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -N mstat
+#PBS -o job_report/mstat.o
+#PBS -e job_report/mstat.e
+
+# PBS -t 1-23
+#PBS -S /bin/bash
+#! Resources requested:
+#PBS -l nodes=1:ppn=8,walltime=10:00:00
+
+mydir="/panfs/panasas01/shared-godmc/godmc_phase2_analysis/01_meta_analysis_16"
+cd $mydir
+
+R CMD BATCH mstat.control.R mstat.Rout
