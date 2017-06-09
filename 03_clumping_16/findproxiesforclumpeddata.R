de6f43f941e613fe12fe29e77ac6d140fd1e3c80
Josine Min <josine.min@bristol.ac.uk>
2017-06-10 00:19:24 +0100

    added Nproxies

--- 03_clumping_16/findproxiesforclumpeddata.R
+++ 03_clumping_16/findproxiesforclumpeddata.R
@@ -11,20 +11,27 @@ cat(i,"\n")
 cl_chr<-cl[which(cl$snpchr%in%paste0("chr",i)),]
 a <- read_tsv(paste0("/panfs/panasas01/shared-godmc/1kg_reference_ph3/eur.filtered.",i,".ld.formatted.gz"))
 a<-data.frame(a)
-a2<-a[which(a$SNP_A%in%cl_chr$snp),]
 
-agg<-a2 %>%  
+agg<-a %>%  
   group_by(BP_A) %>%  
   summarise (start_bp = min(BP_B),  
-             stop_bp = max(BP_B))
+             stop_bp = max(BP_B),nproxies = n())
 
 agg<-data.frame(agg)
+#a2<-a[which(a$SNP_A%in%cl_chr$snp),]
 
 #test<-a3[a3$BP_A=="16855618",]
 #min(test$BP_B)
 #[1] 16855677
 #max(test$BP_B)
 #[1] 16866339
+
+#w<-which(agg$BP_A%in%cl_chr$snppos)
+#> length(w)
+#[1] 3275
+
+#length(which(is.na(cl_chr$BP_A)))
+#[1] 1208
 
 m<-match(cl_chr$snppos,agg$BP_A)
 cl_chr<-data.frame(cl_chr,agg[m,])
