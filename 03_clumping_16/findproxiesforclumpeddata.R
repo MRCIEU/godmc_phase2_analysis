4c52f9c37c9fea66bc5f68e60daa70859b171818
Josine Min <josine.min@bristol.ac.uk>
2017-06-11 01:32:22 +0100

    updated

--- 03_clumping_16/findproxiesforclumpeddata.R
+++ 03_clumping_16/findproxiesforclumpeddata.R
@@ -12,12 +12,48 @@ cl_chr<-cl[which(cl$snpchr%in%paste0("chr",i)),]
 a <- read_tsv(paste0("/panfs/panasas01/shared-godmc/1kg_reference_ph3/eur.filtered.",i,".ld.formatted.gz"))
 a<-data.frame(a)
 
-agg<-a %>%  
+agg1<-a %>%  
   group_by(BP_A) %>%  
-  summarise (start_bp = min(BP_B),  
-             stop_bp = max(BP_B),nproxies = n())
+  summarise (bpa = first(BP_A),start_bp = min(BP_B),  
+             stop_bp = max(BP_B),nproxies = n(),chr=first(CHR_A))
 
-agg<-data.frame(agg)
+agg2<-a %>%  
+  group_by(BP_B) %>%  
+  summarise (bpb = first(BP_B),start_bp = min(BP_A),  
+             stop_bp = max(BP_A),nproxies = n(),chr=first(CHR_B))
+
+b <- full_join(agg1, agg2, c("BP_A" = "BP_B"))
+
+#b$final_min <- b$start_bp.x
+#b$final_max <- b$stop_bp.y
+#b$final_min[is.na(b$final_min)] <- b$bpa[is.na(b$final_min)]
+#b$final_max[is.na(b$final_max)] <- b$bpb[is.na(b$final_max)]
+b<-data.frame(b)
+
+b2<-data.frame(b$nproxies.x,b$nproxies.y)
+b$nproxies<-apply(b2,1,function(x) y<-sum(x,na.rm=T))
+
+b2<-data.frame(b$start_bp.x,b$start_bp.y)
+b$final_min<-apply(b2,1,function(x) y<-min(x,na.rm=T))
+
+b2<-data.frame(b$stop_bp.x,b$stop_bp.y)
+b$final_max<-apply(b2,1,function(x) y<-max(x,na.rm=T))
+
+
+b$CHR<-b$chr.x
+b$CHR[is.na(b$CHR)]<-b$chr.y[is.na(b$CHR)]
+b$CHR<-paste0("chr",b$CHR)
+
+table(is.na(b$final_min))
+table(is.na(b$final_max))
+table(b$final_min <= b$final_max)
+table(b$final_min <= b$final_max)
+
+
+b <- select(b, CHR=CHR,SNP=BP_A, min=final_min, max=final_max,nproxies)
+summary(b$max - b$min)
+
+
 #a2<-a[which(a$SNP_A%in%cl_chr$snp),]
 
 #test<-a3[a3$BP_A=="16855618",]
