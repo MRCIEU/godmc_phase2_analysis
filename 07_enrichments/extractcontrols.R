93e0008d44a230105774cd15677ad7b15da08515
Josine Min <josine.min@bristol.ac.uk>
2017-07-25 15:33:54 +0100

    preparedata

--- 07_enrichments/extractcontrols.R
+++ 07_enrichments/extractcontrols.R
@@ -1,33 +1,33 @@
 path="../results/enrichments"
-l<-list.files(path=path,pattern="controls")
+l<-list.files(path=path,pattern="controlslist")
 
-cis<-grep("controls_cis",l)
-trans<-grep("controls_trans",l)
+cis<-grep("controlslist_cis",l)
+trans<-grep("controlslist_trans",l)
 l1<-l[-c(cis,trans)]
 
 cis<-l[cis]
 trans<-l[trans]
 
-controlset<-NULL
+controlset<-list()
+mqtlset<-NULL
+
+load("../results/enrichments/snpcontrolsets.rdata")
+
 for (i in 1:10){
 
-load(paste(path,"/controls",i,".rdata",sep=""))
+load(paste(path,l1[i],sep="/"))
+mqtlset<-unique(c(mqtlset,unlist(mqtllist)))
+
+snp<-unlist(controllist)
+controlset[[i]]<-f.all[which(f.all$SNP%in%snp),]
 cat(i,"\n")
-
-#first remove rows with NAs only
-
-controls2<-controls[,-1]
-
-ind <- rowSums(is.na(controls2)) == length(controls2) 
-out <- rowSums(controls2, na.rm = TRUE) 
-out[ind] <- NA 
-
-length(which(out==1))
-#[1] 57978
-length(which(out==0))
-#[1] 57978
-
-set<-controls$SNP[which(out==1)]
-controlset<-unique(c(controlset,set))
 }
+
+mqtlset<-f.all[which(f.all$SNP%in%mqtlset),]
+
+dim(mqtlset)
+#[1] 186395
+dim(controlset[[1]])
+
+save(mqtlset,controlset,file="snpsetsforJohanna170724.RData")
 
