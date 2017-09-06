49752aea1bb4c7ab597d0f64f06909835e4e4a69
Gibran Hemani <explodecomputer@gmail.com>
2017-09-06 22:57:08 +0100

    first round of annotations completed

---
+++ 05_cis-trans-networks/scripts/graph_annotations_collate.r
@@ -0,0 +1,38 @@
+library(dplyr)
+
+fn <- paste0("../results/annot_perm", 1:252, ".rdata")
+l <- list()
+for(i in fn)
+{
+	message(i)
+	load(i)
+	l[[i]] <- perms
+}
+perms <- bind_rows(l)
+
+perms$real <- perms$perm == 0
+
+out <- group_by(perms, mem) %>%
+	do({
+		x <- .
+		sig <- sum(x$sig[!x$real] > x$sig[x$real]) / (nrow(x)-1)
+		fdr <- sum(x$fdr[!x$real] > x$fdr[x$real]) / (nrow(x)-1)
+		return(data.frame(sig=sig,fdr=fdr))
+	})
+
+save(perms, file="../results/annot_perms.rdata")
+
+
+
+
+fn <- paste0("../results/annot", 1:252, ".rdata")
+l <- list()
+for(i in fn)
+{
+	message(i)
+	load(i)
+	l[[i]] <- subset(res, pval < 0.01)
+}
+res <- bind_rows(l)
+dim(res)
+save(res, file="../results/annot_tophits.rdata")
