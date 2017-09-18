b51dd8012b9f68e6393de4cbbecb11f7b40f3535
Gibran Hemani <explodecomputer@gmail.com>
2017-09-18 22:49:40 +0100

    performing lola analysis with ext database and proxy ranges for SNPs

---
+++ 05_cis-trans-networks/scripts/lola_followup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=lolaf
+#SBATCH --nodes=1 --mem=60G --ntasks=10 --time=0-20:00:00
+#SBATCH --output=job_reports/slurm-%A.out
+
+echo "Running on ${HOSTNAME}"
+module add R/3.2.3-foss-2016a
+
+Rscript lola_followup.r
+
