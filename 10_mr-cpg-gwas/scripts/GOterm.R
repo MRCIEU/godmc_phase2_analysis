61ce163648941b687225e8ff60c32c3b3fe440db
Josine Min <josine.min@bristol.ac.uk>
2018-06-01 13:22:14 +0100

    goterm enrichments MR

--- 10_mr-cpg-gwas/scripts/GOterm.R
+++ 10_mr-cpg-gwas/scripts/GOterm.R
@@ -12,15 +12,17 @@ go.res[go.res$FDR<0.05,]
 
 go.res2 <- gometh(sig.cpg=cpgs[,1], all.cpg=allcpgs,array.type=c("450K"),collection="KEGG")
 go.res2[which(go.res2$FDR<0.05),]
+#                         Pathway   N DE         P.DE        FDR
+#path:hsa04934 Cushing's syndrome 154  3 0.0001059067 0.03473739
 
 save(go.res,go.res2,file="../results/goenrichments_intracranialvolume.rdata")
 
 
-cpgs<-read.table("../results/intracranial_volume2.txt",sep="\t",he=F,colClasses="character")
+cpgs<-read.table("../results/jia.txt",sep="\t",he=F,colClasses="character")
 go.res <- gometh(sig.cpg=cpgs[,1], all.cpg=allcpgs,array.type=c("450K"))
 go.res[go.res$FDR<0.05,]
 
 go.res2 <- gometh(sig.cpg=cpgs[,1], all.cpg=allcpgs,array.type=c("450K"),collection="KEGG")
 go.res2[which(go.res2$FDR<0.05),]
 
-save(go.res,go.res2,file="../results/goenrichments_intracranialvolume2.rdata")
+save(go.res,go.res2,file="../results/goenrichments_jia.rdata")
