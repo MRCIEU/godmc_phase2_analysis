5ce9db06a87d4aead4d8371c0adede8f0b31b36d
Gibran Hemani <explodecomputer@gmail.com>
2019-01-18 13:25:06 +0000

    updated manhattan

--- 10_mr-cpg-gwas/scripts/plot.r
+++ 10_mr-cpg-gwas/scripts/plot.r
@@ -100,7 +100,8 @@ cats$subcategory[cats$subcategory == "NA"] <- "Anthropometric"
 cats$subcategory[cats$subcategory == "Other"] <- "Metabolite"
 cats$subcategory[cats$subcategory == "Nucleotide"] <- "Metabolite"
 cats$subcategory[cats$subcategory == "Lipid"] <- "Metabolite"
-cats$subcategory[cats$subcategory == "Metal"] <- "Haemotological"
+cats$subcategory[cats$subcategory == "Metal"] <- "Haematological"
+cats$subcategory[cats$subcategory == "Haemotological"] <- "Haematological"
 
 cats <- subset(cats, !duplicated(paste(trait, subcategory))) %>% arrange(trait)
 temp <- merge(temp, cats, by=c("trait"), all.x=TRUE)
@@ -113,6 +114,7 @@ geom_point(data=temp %>% filter(sig), size=2, colour="black", alpha=1) +
 facet_grid(what ~ chr, scale="free", space="free") +
 # scale_colour_manual(values=c("#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd")) +
 # theme_tufte() +
+scale_colour_brewer(palette="PuOr") +
 scale_y_continuous(breaks=seq(0, 100, 20)) +
 labs(x="CpG position", y="-log10 p", colour="") +
 ylim(0, 60) +
@@ -134,13 +136,29 @@ ggsave(file="../images/bidirectional_manhattan2.png", width=10, height=6)
 temp$pval2 <- temp$pval
 temp$pval2[temp$what=="trait to cpg"] <- temp$pval[temp$what=="trait to cpg"] * -1
 temp$chrcol[temp$what=="trait to cpg"] <- !temp$chrcol[temp$what=="trait to cpg"]
-ggplot(temp %>% filter(!sig), aes(x=cpgpos, y=pval2)) +
+temp <- temp %>% arrange(chr, cpgpos)
+temp <- subset(temp, is.finite(pval2))
+
+temp$newpos <- temp$cpgpos
+for(i in 2:23)
+{
+	temp$newpos[temp$chr == i] <- temp$newpos[temp$chr == i] + max(temp$newpos[temp$chr == i-1]) + 10000000 - min(temp$newpos[temp$chr == i])
+}
+
+temp13 <- subset(temp, chr == 13)
+
+ggplot(temp, aes(y=newpos,x=1:nrow(temp))) +
+geom_line(aes(colour=as.factor(chr)))
+
+ggplot(temp %>% filter(!sig), aes(x=newpos, y=pval2)) +	
 geom_point(data=temp %>% filter(chrcol), size=0.2, colour="#bbbbbb") +
-geom_point(data=temp %>% filter(!chrcol), size=0.2, colour="#888888") +
+geom_point(data=temp %>% filter(!chrcol), size=0.2, colour="#dddddd") +
 geom_point(data=temp %>% filter(sig), size=2, colour="black", alpha=1) +
-# geom_point(data=temp %>% filter(sig), size=1, aes(colour=subcategory), alpha=1) +
-facet_grid(. ~ chr, scale="free", space="free") +
+geom_point(data=temp %>% filter(sig), size=1, aes(colour=subcategory), alpha=1) +
+# facet_grid(. ~ chr, scale="free", space="free") +
 # scale_colour_manual(values=c("#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd")) +
+scale_colour_manual(values=c("#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#000000", "#b3de69", "#ffffff", "#d9d9d9", "#bc80bd")) +
+# scale_colour_brewer(palette="BrBg") +
 scale_y_continuous(breaks=seq(-100, 100, 20)) +
 labs(x="CpG position", y="", colour="") +
 ylim(-60, 60) +
