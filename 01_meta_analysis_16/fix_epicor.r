82c0ff945dd33052c413d9a610d557ac118eb739
Josine Min <josine.min@bristol.ac.uk>
2019-02-22 13:23:35 +0000

    updated for GCTA files

--- 01_meta_analysis_16/fix_epicor.r
+++ 01_meta_analysis_16/fix_epicor.r
@@ -3,12 +3,17 @@ suppressWarnings(suppressPackageStartupMessages(library(data.table)))
 
 update_alleles <- function(dat, indels)
 {
+	w<-which(names(dat)%in%c("A1"))
+	if(length(w)==1){names(dat)[w]<-c("EA")}
+	w<-which(names(dat)%in%c("A2"))
+	if(length(w)==1){names(dat)[w]<-c("NEA")}
+
 	dat$index <- 1:nrow(dat)
 	da <- subset(dat, SNP %in% indels$SNP)
 	da <- merge(da, indels, by="SNP")
 	message("Fixing ", nrow(da), " indels")
-	i1 <- da$EA == da$A1.orig
-	i2 <- da$EA == da$A2.orig
+	i1 <- da$EA== da$A1.orig
+	i2 <- da$NEA == da$A2.orig
 	da$EA[i1] <- da$A1[i1]
 	da$NEA[i1] <- da$A2[i1]
 	da$EA[i2] <- da$A1[i2]
